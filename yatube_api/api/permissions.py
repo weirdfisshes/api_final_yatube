1b52cd38137f029716d73733154c103aba0a56b8
Никита Бурцев <nikbur97@yandex.ru>
2022-04-13 15:27:34 +0800

    1.4

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -4,8 +4,10 @@ from rest_framework import permissions
 class AuthorOrReadOnly(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
-        return (request.method in permissions.SAFE_METHODS
-            or obj.author == request.user)
+        return (
+            request.method in permissions.SAFE_METHODS
+            or obj.author == request.user
+        )
 
 
 class ReadOnlyAndIsAuthenticated(permissions.BasePermission):
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets, mixins, permissions, filters
+from rest_framework import viewsets, permissions, filters
+from rest_framework.pagination import LimitOffsetPagination
 from django.shortcuts import get_object_or_404
 
 from posts.models import Post, Group, Follow
@@ -15,6 +16,7 @@ class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = (AuthorOrReadOnly, )
+    pagination_class = LimitOffsetPagination
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -38,15 +40,12 @@ class CommentsViewSet(viewsets.ModelViewSet):
         serializer.save(author=self.request.user, post=post)
 
 
-class FollowViewSet(
-    mixins.CreateModelMixin,
-    mixins.ListModelMixin,
-    viewsets.GenericViewSet
-):
+class FollowViewSet(viewsets.ModelViewSet):
     serializer_class = FollowSerializer
     permission_classes = (permissions.IsAuthenticated,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('user__username', 'following__username',)
+    http_method_names = ('get', 'post')
 
     def get_queryset(self):
         return Follow.objects.filter(user=self.request.user).all()
